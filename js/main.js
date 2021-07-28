84240f2906a6f7a1d4246f8f548c124d43168d04
vyaron <vyaron@gmail.com>
2021-07-28 11:26:26 +0300

    filter works

--- js/main.js
+++ js/main.js
@@ -1,58 +1,49 @@
 
-var gTodos = [
-    {id: 't101', txt: 'Learn HTML', isDone: false},
-    {id: 't102', txt: 'Master CSS', isDone: true}
-]
-
 function onInit() {
     console.log('Hi');
     renderTodos();
 }
 
 function renderTodos() {
-    var elTodoList = document.querySelector('.todo-list');
-    var strHTMLs = gTodos.map(function(todo){
-        return `<li class="${(todo.isDone)? 'done': ''}" onclick="toggleTodo('${todo.id}')">
+    var todos = getTodosForDisplay();
+    var strHTMLs = todos.map(function(todo){
+        return `<li class="${(todo.isDone)? 'done': ''}" onclick="onToggleTodo('${todo.id}')">
             ${todo.txt}
-            <button onclick="removeTodo('${todo.id}', event)">x</button>
+            <button onclick="onRemoveTodo('${todo.id}', event)">x</button>
         </li>`
     })
+
+    var elTodoList = document.querySelector('.todo-list');
     elTodoList.innerHTML = strHTMLs.join('')
 }
 
 
-function toggleTodo(todoId) {
+function onToggleTodo(todoId) {
     console.log('Toggling: ', todoId);
-    var todo = gTodos.find(function(todo){
-        return todo.id === todoId
-    })
-    todo.isDone = !todo.isDone;
+    toggleTodo(todoId)
     renderTodos()
 }
 
-function removeTodo(todoId, ev) {
-    ev.stopPropagation();
-
+function onRemoveTodo(todoId, ev) {
     console.log('Removing: ', todoId);
-    var idx = gTodos.findIndex(function(todo){
-        return todo.id === todoId
-    })
-    gTodos.splice(idx, 1);
+    ev.stopPropagation();
+    removeTodo(todoId);
     renderTodos();
 }
 
-function addTodo() {
+function onAddTodo() {
     var elTxt = document.querySelector('[name=newTodoTxt]');
     var txt = elTxt.value;
     if (!txt) return;
 
-    var todo = {
-        id : 't' + (Date.now() % 100),
-        txt: txt,
-        isDone: false
-    }
-    gTodos.unshift(todo);
+    addTodo(txt)
     elTxt.value = '';
     
+    renderTodos();
+}
+
+function onSetFilter(filterBy) {
+    console.log('Filtering by:', filterBy);
+    setFilterBy(filterBy);
     renderTodos();
 }
---
+++ js/services/todo.service.js
@@ -0,0 +1,47 @@
+var gTodos = [
+    {id: 't101', txt: 'Learn HTML', isDone: false},
+    {id: 't102', txt: 'Master CSS', isDone: true},
+    {id: 't103', txt: 'Do JS', isDone: false}
+]
+
+var gFilterBy = 'all'
+
+
+function getTodosForDisplay() {
+    if (gFilterBy === 'all')  return gTodos;
+    
+    var todos = gTodos.filter(function(todo){
+        return (gFilterBy === 'active' && !todo.isDone) || 
+               (gFilterBy === 'done' && todo.isDone)
+
+    })
+    return todos;
+}
+
+
+function removeTodo(todoId) {
+    var idx = gTodos.findIndex(function(todo){
+        return todo.id === todoId
+    })
+    gTodos.splice(idx, 1);
+}
+
+function addTodo(txt) {
+    var todo = {
+        id : 't' + (Date.now() % 100),
+        txt: txt,
+        isDone: false
+    }
+    gTodos.unshift(todo);
+}
+
+function toggleTodo(todoId) {
+    var todo = gTodos.find(function(todo){
+        return todo.id === todoId
+    })
+    todo.isDone = !todo.isDone;
+}
+
+function setFilterBy(filterBy) {
+    gFilterBy = filterBy
+}
